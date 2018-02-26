a4a163585068fbee239fe5b67ee412026a4c43d0
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-02-26 14:05:42 -0800

    Initial connection established
    dependencies downloaded
    data structure draft built in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,10 @@
 // this is where the friends a stored
 // and where compatibility logic runs
+
+var friendsArray = [
+    {
+        name: "Jeremy",
+        photo: "https://goo.gl/images/Wt1Q9o",
+        scores: [1, 2, 2, 1, 3]
+    }
+]
--- server.js
+++ server.js
@@ -2,3 +2,26 @@
 // captures survey input, 
 // makes the ajax call to 
 // and displays the results modal
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.get("/", function (req, res){
+    res.send("Hello World");
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(function (req, res){
+    res.setHeader("Content_Type", "text/plain")
+    res.write("you posted:\n")
+    res.end(JSON.stringify(req.body. null, 2))
+})
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT)
+});
